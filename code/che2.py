1c20eed8dc21e16fc34ba3bd47a9a18f1f0dc6df
unknown <kalybekovilim8@gmail.com>
2023-11-22 23:37:35 +0600

    Pythontuter

--- code/che2.py
+++ code/che2.py
@@ -1,3 +1,11 @@
-print("Goodby world")
-
-print("5day")
+# print("Goodby world")
+#
+# print("5day")
+#
+# d = input()
+# print("Hello",d)
+a = int(input())
+d = f"The next number for the number ${a} + is ${a + 1}"
+# s = "The next number for the number" input() "is" input() - 1
+print(d)
+# print(s)
