f415075e66924eec4fae2eeba8c0de336f3c5c2c
unknown <kalybekovilim8@gmail.com>
2023-11-23 21:08:01 +0600

    soft other problems

--- code/che2.py
+++ code/che2.py
@@ -4,8 +4,19 @@
 #
 # d = input()
 # print("Hello",d)
-a = int(input())
-d = f"The next number for the number ${a} + is ${a + 1}"
-# s = "The next number for the number" input() "is" input() - 1
-print(d)
-# print(s)
+# a = int(input())
+# d = f"The next number for the number ${a} + is ${a + 1}"
+# # s = "The next number for the number" input() "is" input() - 1
+# print(d)
+# # print(s)
+# d = f"The next number for the number" + a + "is" + a + 1
+# s = f"The next number for the number" + a + "is" + a - 1
+
+# a = int(input())
+# s = int(input())
+# d = int(input())
+# print((69 // 2) + (69 % 2) * 2)
+
+print((17 // 2 + 17 % 2) + (19 // 2 + 19 % 2) + (18 // 2 + 18 % 2))
+# print(19 // 2)
+# print(18 // 2)
